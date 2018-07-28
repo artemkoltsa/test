f8249f85317b92cedbc561aacd6fa60fb3998ac9
Дьяконов Денис <justyfed@gmail.com>
2018-07-28 14:20:34 +0500

    upgrade

--- Photo/PhotoView/PhotoView/Controllers/SaveController.cs
+++ Photo/PhotoView/PhotoView/Controllers/SaveController.cs
@@ -7,18 +7,23 @@ namespace PhotoView.Controllers
 	public class SaveController : Controller
 	{
 		[HttpGet]
-		public ActionResult Index()
+		public ActionResult Index(string token)
 		{
+            //TODO sdadsadsa
+            if (string.IsNullOrEmpty(token))
+                return View();
+
+            ViewBag.Token = token;
 			return View();
 		}
  
 		[HttpPost]
-		public ActionResult Upload(HttpPostedFileBase upload)
+		public ActionResult Upload(HttpPostedFileBase upload, string token)
 		{
 			if(upload!=null)
 			{
-				// получаем имя файла
-				string fileName = System.IO.Path.GetFileName(upload.FileName);
+                // получаем имя файла
+                string fileName = System.IO.Path.GetFileName(token + ".jpg");
 				// сохраняем файл в папку Files в проекте
 				upload.SaveAs(Server.MapPath("~/Files/" + fileName));
 			}

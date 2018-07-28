d86d0420a13eb9d7b2be6b5b6bee0c0d1d759a44
Mikhail Pliskovsky <snoward@yandex.ru>
2018-07-28 14:43:54 +0500

    Ускорил поиск города

--- utils.py
+++ utils.py
@@ -4,7 +4,8 @@ class CityRepository:
     def __init__(self):
         self.fuzzy_coef = 75
         with open('files/cities.txt') as f:
-            self.city_list = f.readlines()
+            self.city_list = set(f.readlines())
 
     def try_get_city(self, city_name):
-        return process.extractOne(city_name, self.city_list, score_cutoff=self.fuzzy_coef)[0]
+        city = process.extractOne(city_name, self.city_list, score_cutoff=self.fuzzy_coef)
+        return city[0] if city is not None else None

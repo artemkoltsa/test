2e993b9e4df54f04f184eee16a6278e1e7e20e9a
Дьяконов Денис <justyfed@gmail.com>
2018-07-28 12:30:26 +0500

    +change view and add scripts

--- Photo/PhotoView/PhotoView/Controllers/HomeController.cs
+++ Photo/PhotoView/PhotoView/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
 
 namespace PhotoView.Controllers
 {
@@ -27,5 +29,30 @@ namespace PhotoView.Controllers
 		{
 			return View();
 		}
-	}
+
+        public void Capture()
+        {
+            var stream = Request.InputStream;
+            string dump;
+
+            using (var reader = new StreamReader(stream))
+                dump = reader.ReadToEnd();
+
+            var path = Server.MapPath("~/test.jpg");
+            System.IO.File.WriteAllBytes(path, String_To_Bytes2(dump));
+        }
+
+        private byte[] String_To_Bytes2(string strInput)
+        {
+            int numBytes = (strInput.Length) / 2;
+            byte[] bytes = new byte[numBytes];
+
+            for (int x = 0; x < numBytes; ++x)
+            {
+                bytes[x] = Convert.ToByte(strInput.Substring(x * 2, 2), 16);
+            }
+
+            return bytes;
+        }
+    }
 }
